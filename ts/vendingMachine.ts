424bc077214fabca588dea3caf713f7517441e1f
Plastikaweb <info@plastikaweb.com>
2016-08-07 17:17:34 +0200

    generics , decorados...

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -15,15 +15,15 @@ class Cell {
     constructor(public product: Product.Product) {
 
     }
-    stock = ko.observable(3);
-    sold = ko.observable(false);
+    stock = ko.observable<number>(3);
+    sold = ko.observable<boolean>(false);
 }
 
 export class VendingMachine {
     private paid = ko.observable(0);
     selectedCell = ko.observable(new Cell(new Product.CocaCola()));
     cells = ko.observableArray([]);
-    acceptedCoins: Coins.Coin[] = [new Coins.Dime(), new Coins.Quarter(), new Coins.Half(), new Coins.Dollar()];
+    acceptedCoins: Array<Coins.Coin> = [new Coins.Dime(), new Coins.Quarter(), new Coins.Half(), new Coins.Dollar()];
     canPay = ko.pureComputed(() => this.paid() -
         this.selectedCell().product.price >= 0);
 
